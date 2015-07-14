6dfec6c7a86aa5dee5c6b7f99d8549a1a1f0b4f1
Sergey Vartanov <me@enzet.ru>
2015-07-14 12:01:20 +0300

    Stairs detection.

--- svg_to_shape.py
+++ svg_to_shape.py
@@ -5,14 +5,13 @@
 import copy
 import re
 import sys
+from xml.dom import minidom
 
 sys.path.append('lib')
 
 import path
 import svg
-import vector
-
-from xml.dom import minidom
+from vector import Vector, Matrix3
 
 usage = 'python ' + sys.argv[0] + ' <input SVG file> <output shape file>'
 debug_svg_file_name = 'debug.svg'
@@ -38,52 +37,109 @@ def repr(element):
     return s
 
 
+def extract_path(element, matrix):
+    """
+    Extract path from SVG <path> element.
+    """
+    current_path = element.attributes['d'].value
+    p = path.Path()
+    p.parse_from_svg(current_path)
+    p.transform(matrix)
+    return p
+
+
+def get_bounds(paths):
+
+    def get_for(get_component):
+        min_c, p_min = 1000, None
+        max_c, p_max = -1000, None
+        for p in paths:
+            for s in p.segments:
+                if get_component(s.start) < min_c: 
+                    min_c, p_min = get_component(s.start), p
+                if get_component(s.start) > max_c: 
+                    max_c, p_max = get_component(s.start), p
+                if get_component(s.end) < min_c: 
+                    min_c, p_min = get_component(s.end), p
+                if get_component(s.end) > max_c: 
+                    max_c, p_max = get_component(s.end), p
+        return min_c, max_c, p_min, p_max
+
+    min_x, max_x, p_min, p_max = get_for(lambda a: a.x)
+    if p_min == p_max:
+        min_y, max_y, p_min, p_max = get_for(lambda a: a.y)
+
+    return p_min, p_max
+
+
 def process(root, level, matrix):
+    """
+    Process SVG element.
+    """
     if isinstance(root, minidom.Text):
         return
     # print (' ' * (level * 2)) + repr(root.localName)
+
+    # Transformation
+
     if 'transform' in root.attributes.keys():
         new_matrix = None
-        m = re.match('^translate\((?P<x>[0-9.-]*),(?P<y>[0-9.-]*)\)$', root.attributes['transform'].value)
+        m = re.match('^translate\((?P<x>[0-9.-]*),(?P<y>[0-9.-]*)\)$', \
+            root.attributes['transform'].value)
         if m:
-            new_matrix = vector.Matrix3([[1, 0, float(m.group('x'))],
-                                        [0, 1, float(m.group('y'))],
-                                        [0, 0, 1]])
-        m = re.match('^matrix\((?P<a>[0-9.-]*),(?P<b>[0-9.-]*),(?P<c>[0-9.-]*),(?P<d>[0-9.-]*),(?P<e>[0-9.-]*),(?P<f>[0-9.-]*)\)$', root.attributes['transform'].value)
+            new_matrix = Matrix3([[1, 0, float(m.group('x'))],
+                                  [0, 1, float(m.group('y'))],
+                                  [0, 0, 1]])
+        m = re.match('^matrix\((?P<a>[0-9.-]*),(?P<b>[0-9.-]*),' + \
+                     '(?P<c>[0-9.-]*),(?P<d>[0-9.-]*),(?P<e>[0-9.-]*),' + \
+                     '(?P<f>[0-9.-]*)\)$', root.attributes['transform'].value)
         if m:
-            new_matrix = vector.Matrix3([[float(m.group('a')), float(m.group('c')), float(m.group('e'))],
-                                        [float(m.group('b')), float(m.group('d')), float(m.group('f'))],
-                                        [0, 0, 1]])
+            new_matrix = Matrix3([[float(m.group('a')), float(m.group('c')), float(m.group('e'))],
+                                  [float(m.group('b')), float(m.group('d')), float(m.group('f'))],
+                                  [0, 0, 1]])
         if new_matrix:
             matrix = copy.deepcopy(matrix) * new_matrix
+
+    str_matrix = None
+    if matrix:
+        str_matrix = 'matrix(' + str(matrix.matrix[0][0]) + ',' + \
+                                 str(matrix.matrix[1][0]) + ',' + \
+                                 str(matrix.matrix[0][1]) + ',' + \
+                                 str(matrix.matrix[1][1]) + ',' + \
+                                 str(matrix.matrix[0][2]) + ',' + \
+                                 str(matrix.matrix[1][2]) + ')'
+
+    # Element detection
+
     if root.localName == 'g':
         a = 0
         for child in root.childNodes:
             if child.localName == 'path':
                 a += 1
-        if a == 8 or a == 3:
+        if a == 10 or a == 3:
             print 'stairs'
-    if root.localName == 'path':
-        str_matrix = None
-        if matrix:
-            str_matrix = 'matrix(' + str(matrix.matrix[0][0]) + ',' + str(matrix.matrix[1][0]) + ',' + \
-                                                                      str(matrix.matrix[0][1]) + ',' + str(matrix.matrix[1][1]) + ',' + \
-                                                                      str(matrix.matrix[0][2]) + ',' + str(matrix.matrix[1][2]) + ')'
-        current_path = root.attributes['d'].value
-        p = path.Path()
-        p.parse_from_svg(current_path)
-        p.transform(matrix)
-        fixed_path = p.to_svg()
-        output.path(current_path, transform=str_matrix, width=2)
-        output.path(fixed_path, color='FF0000')
-    for child in root.childNodes:
-        process(child, level + 1, matrix)
+            paths = []
+            for child in root.childNodes:
+                if child.localName == 'path':
+                    paths.append(extract_path(child, matrix))
+            p_min, p_max = get_bounds(paths)
+            output.path(p_min.to_svg(), color='000000')
+            output.path(p_max.to_svg(), color='000000')
+        else:
+            for child in root.childNodes:
+                process(child, level + 1, matrix)
+    elif root.localName == 'path':
+        p = extract_path(root, matrix)
+        output.path(p.to_svg(), color='FF0000')
+    else:
+        for child in root.childNodes:
+            process(child, level + 1, matrix)
 
 
 output = svg.SVG(open(debug_svg_file_name, 'w+'))
 
 output.begin(1000, 1000)
 
-process(inner_svg, 0, vector.Matrix3([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+process(inner_svg, 0, Matrix3([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
 
 output.end()
